879a77b220ed0f7526e3fb16fbf4e22fad36f528
简涛 <jiantao@pp100.com>
2018-08-17 17:45:09 +0800

    like epsoide component

--- components/like/like.js
+++ components/like/like.js
@@ -1,23 +1,36 @@
 // components/like/like.js
 Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        isLike: {
+            type: Boolean,
+            value: true
+        },
+        count: {
+            type: Number,
+            value: 20
+        }
+    },
 
-  },
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        yesUrl: "../images/like.png",
+        noUrl:'../images/like@dis.png'
+    },
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
-  }
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+      onlike:function(){
+        this.setData({
+          isLike:!this.properties.isLike,
+          count:!this.properties.isLike?this.properties.count+1:this.properties.count-1
+        })
+      }
+    }
 })
